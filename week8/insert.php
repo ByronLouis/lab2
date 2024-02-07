3c8e78c49c25df91e0b75b11b41b5717ace9f664
Byron Louis Rabajante <byronrabajante@gmail.com>
2024-02-07 14:52:20 +0800

    qwerty

--- week8/insert.php
+++ week8/insert.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+} else(echo "connection success")
 
 $sql = "INSERT INTO barabajante3_myguests (name, email, website, comment, gender)
 VALUES ('$name', '$email', '$website', '$comment', '$gender')";
