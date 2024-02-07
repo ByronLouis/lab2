4714fbfa89a5059d0cb5e0fab2ba32e93f6e9178
Byron Louis Rabajante <byronrabajante@gmail.com>
2024-02-07 14:55:46 +0800

    q123ds

--- week8/insert.php
+++ week8/insert.php
@@ -7,21 +7,21 @@ $dbname = "webprogmi222_sf221";
 
 include 'index.php'
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} else(echo "connection success")
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// } else(echo "connection success")
 
-$sql = "INSERT INTO barabajante3_myguests (name, email, website, comment, gender)
-VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+// $sql = "INSERT INTO barabajante3_myguests (name, email, website, comment, gender)
+// VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+echo '$name';
+// $conn->close();
 // header("Location: index.php")
 ?>
